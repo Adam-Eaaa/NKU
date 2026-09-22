e7803a634032c87b8ecdc2cf3037245b25aa3a79
Adam-Miao <adammiao@foxmail.com>
2026-09-22 08:22:29 +0800

    Sept.22

--- G1F/ProgramFundamentals_HW/HW/Chap2/1.cpp
+++ G1F/ProgramFundamentals_HW/HW/Chap2/1.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main( ) {
+    double radius = 0, height = 0, area = 0, volume = 0;
+    const double PI = 3.14;
+    scanf("%lf%lf", &radius, &height);
+    area = PI * 2 * radius * (radius + height);
+    volume = PI * radius * radius * height;
+    printf("Area: %.3lf\tVolume: %.3lf\n", area, volume);
     return 0;
 }
---
+++ G1F/ProgramFundamentals_HW/HW/Chap2/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double C1 = 0.15, C2 = 0.96;
+    double mINIT = 6880;
+    double nH2SO4 = mINIT * C1;
+    double mCONC = nH2SO4 / C2;
+    cout << mCONC;
+    return 0;
+}
