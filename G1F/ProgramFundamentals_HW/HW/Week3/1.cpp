30d93cdede93e18b28bc636ce2a14acf76a9a11c
Adam-Miao <adammiao@foxmail.com>
2026-09-26 23:32:54 +0800

    Sept.26

--- G1F/ProgramFundamentals_HW/HW/Week3/1.cpp
+++ G1F/ProgramFundamentals_HW/HW/Week3/1.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// 没错, 实现原理是 快速写(龟速乘(快速读(), 快速读()))
+// 只是因为写成一行看着很爽 ( )
+// 那为什么还要专门写龟速乘？
+// 别管，就说能不能跑吧
+
 inline int read( ) {
     int res = 0;
     bool symb = true;
